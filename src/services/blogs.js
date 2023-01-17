b41cb47e8f6db018ef15f17ea205cbf829873207
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-17 14:19:02 -0800

    update BlogCard to include author's name

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,6 @@
 import { checkError, client } from './client.js';
 
 export async function getBlogs() {
-  const resp = await client.from('blogs').select('*');
+  const resp = await client.from('blogs').select('*, authors(*)');
   return checkError(resp);
 }
